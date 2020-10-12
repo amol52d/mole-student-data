95585cf8e685d5614fdb3ffd8d6a9831254809fd
nibba <nibba@gwenchana.xyz>
2020-10-12 17:58:57 +0530

    Imports Optimizations

--- src/main/java/com/student/data/controllers/IndexController.java
+++ src/main/java/com/student/data/controllers/IndexController.java
@@ -2,12 +2,8 @@ package com.student.data.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Amol Dhekane
--- src/main/java/com/student/data/controllers/StudentController.java
+++ src/main/java/com/student/data/controllers/StudentController.java
@@ -3,16 +3,12 @@ package com.student.data.controllers;
 import com.student.data.dao.Student;
 import com.student.data.services.StudentService;
 import com.student.data.utility.StudentSave;
-import com.student.data.utility.StudentSearchById;
 import com.student.data.utility.model.UniversalResponsePayload;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
